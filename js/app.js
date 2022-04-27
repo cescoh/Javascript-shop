29078fac5e5d401d1ee4a24aa13319d4897c984b
pazfrancisco86 <pazfrancisco86@gmail.com>
2022-04-27 00:02:54 -0300

    2daentrega

---
+++ js/app.js
@@ -0,0 +1,126 @@
+let carritoDeCompras = [];
+
+const contenedorPozo = document.getElementById("contenedor-pozo");
+const contenedorCarrito = document.getElementById('carrito-contenedor');
+const sumaTotal = document.getElementById('sumatotal');
+const contadorItems = document.getElementById('contador');
+const legajoTotal = document.getElementById('totaLegajo');
+const perfilesTotal = document.getElementById('totalPerfiles');
+const filtroArea = document.getElementById("areaS");
+/* const formulario = document.getElementById("formulario");
+const boton = document.getElementById("buscar"); */
+
+
+//logica BuscadordeInfo
+//filtro
+
+mostrarInfo(pozosDB);
+
+filtroArea.addEventListener('change',()=>{
+    if(filtroArea.value == "all"){
+        mostrarInfo(pozosDB);
+    }else{
+        mostrarInfo(pozosDB.filter(elemento => elemento.Area == filtroArea.value));
+    }
+});
+
+function mostrarInfo(array) {
+    contenedorPozo.innerHTML = "";
+
+    array.forEach(item => {
+        let div = document.createElement('div')
+        div.classList.add('Pozo')
+        div.innerHTML += `
+        <div class="card" style="width: 18rem;">
+        <img src="${item.img}" class="card-img-top" alt="100anios">
+        <div class="card-body">
+            <h5 class="card-title">${item.NombreLegal}${item.shortN}${item.Numero}</h5>
+            <p class="card-text">Agrega al carrito para solicitar la informacion de este pozo.</p>
+            <p>Legajo: ${item.Legajo}</p>
+            <p>Perfiles: ${item.Perfiles}</p>
+            <p>Disponible: ${item.enStock}</p>
+            <a href="#" id="Agregar${item.id}" class="btn btn-primary">Agregar a carrito</a>
+        </div>
+        </div>`
+        contenedorPozo.appendChild(div) //AGREGA LA INFO DEL ARRAY AL ESQUEMA CARD
+        let btnAgregar = document.getElementById(`Agregar${item.id}`) //LE DA IDENTIDAD AL BOTON AGREGAR A CARRITO CON EL ID DEL POZO
+        btnAgregar.addEventListener("click", () => {
+            agregarAlCarrito(item.id);
+        })
+    });
+}
+
+function agregarAlCarrito(id) {
+    let yaEsta = carritoDeCompras.find(item=> item.id ==id)
+    if(yaEsta){
+        Toastify({
+            text: "Ya tienes esta informacion en el carrito",
+            duration: 3000,
+            newWindow: true,
+            close: true,
+            gravity: "top",
+            position: "right",
+            stopOnFocus: true,
+            style: {
+              background: "linear-gradient(to right, 06006e, #000000,#007a93)",
+            },
+            onClick: function(){}
+          }).showToast();
+        return;
+    } else{
+    let productoAgregar = pozosDB.find(elemento => elemento.id == id);
+    carritoDeCompras.push(productoAgregar);
+    actualizarCarrito()
+    mostrarCarrito(productoAgregar);
+}
+localStorage.setItem('Pedido', JSON.stringify(carritoDeCompras));
+}
+
+function mostrarCarrito(productoAgregar) {
+    let tr = document.createElement('tr');
+    tr.className = "productoEnCarrito";
+    tr.innerHTML = `
+                    <th scope="row">${productoAgregar.NombreLegal+productoAgregar.shortN+productoAgregar.Numero}</th>
+                    <td>${productoAgregar.Legajo}</td>
+                    <td>${productoAgregar.Perfiles}</td>
+                    <button id="eliminar${productoAgregar.id}" type="button" class="btn btn-danger">Quitar</button></td>
+                    <td><span>${productoAgregar.Perfiles+productoAgregar.Legajo}</span></td>
+    `
+    contenedorCarrito.appendChild(tr);
+
+    let btnEliminar = document.getElementById(`eliminar${productoAgregar.id}`);
+
+    btnEliminar.addEventListener('click', () => {
+        btnEliminar.parentElement.remove()
+        carritoDeCompras = carritoDeCompras.filter(item=> item.id != productoAgregar.id)
+        actualizarCarrito()
+        localStorage.setItem('Pedido', JSON.stringify(carritoDeCompras));
+    })
+    let btnLimpiar = document.getElementById('limpiar');
+    /* btnLimpiar.addEventListener('click',()=> {
+        document.getElementById('carrito-contenedor').remove();
+        carritoDeCompras = [];
+        actualizarCarrito();
+        localStorage.setItem('Pedido', JSON.stringify(carritoDeCompras));
+    }) */
+}
+
+
+function actualizarCarrito() {
+    contadorItems.innerText = carritoDeCompras.length;
+    legajoTotal.innerText = carritoDeCompras.reduce((acc, el) => acc + el.Legajo, 0);
+    perfilesTotal.innerText = carritoDeCompras.reduce((acc,el) => acc + el.Perfiles, 0);
+    sumaTotal.innerText = carritoDeCompras.reduce((acc,el) => acc + (el.Legajo+el.Perfiles), 0);
+}
+
+function recuperar() {
+ let recuperarLS = JSON.parse(localStorage.getItem('Pedido'));
+ if(recuperarLS){
+     recuperarLS.forEach(el=>{
+         mostrarCarrito(el)
+         carritoDeCompras.push(el)
+         actualizarCarrito()
+     })
+ }
+}
+recuperar()
