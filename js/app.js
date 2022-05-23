8dbeb731f60c25cc1e13f9a29a0a42a2bd3b4d70
pazfrancisco86 <pazfrancisco86@gmail.com>
2022-05-23 18:39:04 -0300

    PROYECTOFINALSINCSS

--- js/app.js
+++ js/app.js
@@ -7,70 +7,111 @@ const contadorItems = document.getElementById('contador');
 const legajoTotal = document.getElementById('totaLegajo');
 const perfilesTotal = document.getElementById('totalPerfiles');
 const filtroArea = document.getElementById("areaS");
-/* const formulario = document.getElementById("formulario");
-const boton = document.getElementById("buscar"); */
+document.addEventListener("DOMContentLoaded", () => {
+    fetchData();
+})
 
 
-//logica BuscadordeInfo
-//filtro
+const fetchData = async () => {
+        const res = await fetch("/stockProductos.json")
+        const data = await res.json();
+        mostrarInfo(data);
+}
 
-mostrarInfo(pozosDB);
-
-//UTILIZACION DE OPERADOR TERNARIO
-filtroArea.addEventListener('change', () => {
-    filtroArea.value == "all" ? mostrarInfo(pozosDB) : mostrarInfo(pozosDB.filter(elemento => elemento.Area == filtroArea.value))
-});
-
-function mostrarInfo(array) {
+const mostrarInfo = async (data) => {
     contenedorPozo.innerHTML = "";
-
-    array.forEach(item => {
+    data.forEach(item => {
         let div = document.createElement('div')
         div.classList.add('Pozo')
         div.innerHTML += `
-        <div class="card" style="width: 18rem;">
-        <img src="${item.img}" class="card-img-top" alt="100anios">
-        <div class="card-body">
-            <h5 class="card-title">${item.NombreLegal}${item.shortN}${item.Numero}</h5>
-            <p class="card-text">Agrega al carrito para solicitar la informacion de este pozo.</p>
-            <p>Legajo: ${item.Legajo}</p>
-            <p>Perfiles: ${item.Perfiles}</p>
-            <p>Disponible: ${item.enStock}</p>
-            <a href="#" id="Agregar${item.id}" class="btn btn-primary">Agregar a carrito</a>
-        </div>
-        </div>`
+            <div class="card" style="width: 18rem;">
+            <img src="${item.img}" class="card-img-top" alt="100anios">
+            <div class="card-body">
+                <h5 class="card-title">${item.NombreLegal}${item.shortN}${item.Numero}</h5>
+                <p class="card-text">Agrega al carrito para solicitar la informacion de este pozo.</p>
+                <p>Legajo: ${item.Legajo}</p>
+                <p>Perfiles: ${item.Perfiles}</p>
+                <p>Disponible: ${item.enStock}</p>
+                <a href="#" id="Agregar${item.id}" class="btn btn-primary">Agregar a carrito</a>
+            </div>
+            </div>`
         contenedorPozo.appendChild(div) //AGREGA LA INFO DEL ARRAY AL ESQUEMA CARD
         let btnAgregar = document.getElementById(`Agregar${item.id}`) //LE DA IDENTIDAD AL BOTON AGREGAR A CARRITO CON EL ID DEL POZO
-        btnAgregar.addEventListener("click", () => {
+        btnAgregar.addEventListener("click", (e) => {
+            e.preventDefault();
             agregarAlCarrito(item.id);
         })
+    })
+}
+
+const filtroA = async () => {
+    contenedorPozo.innerHTML = "";
+    const resp = await fetch("stockProductos.json");
+    const data = await resp.json();
+
+    const filtroArea = document.getElementById("areaS");
+    filtroArea.addEventListener('change', () => {
+        if (filtroArea.value == "all") {
+            mostrarInfo(data);
+            return;
+        }
+        if (filtroArea.value) {
+            contenedorPozo.innerHTML = "";
+            let filtro = data.filter(item => item.AreaID == filtroArea.value)
+            filtro.forEach(Pozo => {
+                let div = document.createElement('div')
+                div.classList.add('Pozo')
+                div.innerHTML += `
+            <div class="card" style="width: 18rem;">
+            <img src="${Pozo.img}" class="card-img-top" alt="100anios">
+            <div class="card-body">
+                <h5 class="card-title">${Pozo.NombreLegal}${Pozo.shortN}${Pozo.Numero}</h5>
+                <p class="card-text">Agrega al carrito para solicitar la informacion de este pozo.</p>
+                <p>Legajo: ${Pozo.Legajo}</p>
+                <p>Perfiles: ${Pozo.Perfiles}</p>
+                <p>Disponible: ${Pozo.enStock}</p>
+                <a href="#" id="Agregar${Pozo.id}" class="btn btn-primary">Agregar a carrito</a>
+            </div>
+            </div>`
+                contenedorPozo.appendChild(div) //AGREGA LA INFO DEL ARRAY AL ESQUEMA CARD
+                let btnAgregar = document.getElementById(`Agregar${Pozo.id}`) //LE DA IDENTIDAD AL BOTON AGREGAR A CARRITO CON EL ID DEL POZO
+                btnAgregar.addEventListener("click", (e) => {
+                    e.preventDefault();
+                    agregarAlCarrito(Pozo.id);
+                })
+            })
+        }
     });
 }
 
-function agregarAlCarrito(id) {
-    let yaEsta = carritoDeCompras.find(item => item.id == id)
-    if (yaEsta) {
-        Toastify({
-            text: "Ya tienes esta informacion en el carrito",
-            duration: 3000,
-            newWindow: true,
-            close: true,
-            gravity: "top",
-            position: "right",
-            stopOnFocus: true,
-            style: {
-                background: "linear-gradient(to right, 06006e, #000000,#007a93)",
-            },
-            onClick: function () {}
-        }).showToast();
-        return;
-    } else {
-        let productoAgregar = pozosDB.find(elemento => elemento.id == id);
-        carritoDeCompras.push(productoAgregar);
-        actualizarCarrito()
-        mostrarCarrito(productoAgregar);
-    }
-    localStorage.setItem('Pedido', JSON.stringify(carritoDeCompras));
+filtroA();
+
+const agregarAlCarrito = async (id) => {
+        const res = await fetch("/stockProductos.json")
+        const data = await res.json();
+        let yaEsta = carritoDeCompras.find(item => item.id == id);
+        if (yaEsta) {
+            Toastify({
+                text: "Ya tienes esta informacion en el carrito",
+                duration: 3000,
+                newWindow: true,
+                close: true,
+                gravity: "top",
+                position: "right",
+                stopOnFocus: true,
+                style: {
+                    background: "linear-gradient(to right, 06006e, #000000,#007a93)",
+                },
+                onClick: function () {}
+            }).showToast();
+            return;
+        } else {
+            let productoAgregar = data.find(elemento => elemento.id == id);
+            carritoDeCompras.push(productoAgregar);
+            actualizarCarrito()
+            mostrarCarrito(productoAgregar);
+            localStorage.setItem('Pedido', JSON.stringify(carritoDeCompras));
+        }
 }
 
 function mostrarCarrito(productoAgregar) {
@@ -124,7 +165,7 @@ function mostrarCarrito(productoAgregar) {
 
 function actualizarCarrito() {
     contadorItems.innerText = carritoDeCompras.length;
-    carritoDeCompras.length == 4 && Swal.fire('Realmente necesitas tanto?', 'Por favor, evita la acumulacion de informacion', 'warning');//OPERADOR LOGICO &&
+    carritoDeCompras.length == 4 && Swal.fire('Realmente necesitas tanto?', 'Por favor, evita la acumulacion de informacion', 'warning'); //OPERADOR LOGICO &&
     legajoTotal.innerText = carritoDeCompras.reduce((acc, el) => acc + el.Legajo, 0);
     perfilesTotal.innerText = carritoDeCompras.reduce((acc, el) => acc + el.Perfiles, 0);
     sumaTotal.innerText = carritoDeCompras.reduce((acc, el) => acc + (el.Legajo + el.Perfiles), 0);
